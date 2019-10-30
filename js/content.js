19a67eb03fb68cf9089cd94f7769ad4fbed2a2f6
fujita25 <57166196+fujita25@users.noreply.github.com>
2019-10-30 13:59:42 +0900

    file to upload

---
+++ js/content.js
@@ -0,0 +1,35 @@
+var query = location.search;
+var value = query.split('=');
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "../data/ikeda.csv", true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+ 
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+    var spotList=document.getElementById("main");
+    spotList.innerHTML+="<a class='a2'>"+result[value[1]][2]+"</a></br>";
+    spotList.innerHTML+="<img src='../img/mp/"+result[value[1]][3]+"' width='100%'></br>";
+    spotList.innerHTML+="<br><div class='box25'>"+result[value[1]][6]+"</div></br>";
+    for(var k=7;k<result[value[1]].length;++k){
+    spotList.innerHTML+="<li>"+result[value[1]][k]+"</li></br>";
+    }
+}
+ 
+getCSV(); 
+
